a88ff25620e22516549143ba1f3d4607afccc33e
Lenzjo <lenzjo@gmail.com>
2019-08-07 14:57:29 +0100

    Moved sensor read to new file

--- src/main.cpp
+++ src/main.cpp
@@ -33,31 +33,3 @@ void loop() {
 }
 
 
-void refresh_readings() {
-  float f_temperature;
-  float f_humidity;
-  float f_pressure;
-  float f_altitude;
-
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  f_temperature = bme.readTemperature();
-  f_humidity    = bme.readHumidity();
-  f_pressure    = bme.readPressure();
-  f_altitude    = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-  Serial.print(f_temperature);
-  Serial.println(" 'C");
-
-  Serial.print(f_humidity);
-  Serial.println(" %");
-
-  Serial.print(f_pressure);
-  Serial.println(" hPa");
-
-  Serial.print(f_altitude);
-  Serial.println(" m");
-
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("-------------");
-}
